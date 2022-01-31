9f4d9f4b8621ff05e81ee0388af5cf880f271dfb
wrait <wrait@HOME-PC>
2022-01-31 12:07:06 +0100

    Add project files.

---
+++ 3DRenderer/main.cpp
@@ -0,0 +1,26 @@
+// Engine:
+// 3DRenderer	[x]
+// Physics		[]
+// Audio		[]
+// Controls		[]
+// (Networking)	[]
+//
+
+enum class GameState {
+
+};
+
+int APIENTRY wWinMain(
+	_In_     HINSTANCE   hInstance,
+	_In_opt_ HINSTANCE   hPrevInstance,
+	_In_     LPWSTR      lpCmdLine,
+	_In_     int         nCmdShow)
+{
+	//Settup
+
+	while (1) 
+	{
+
+	}
+	return 0;
+}
